a6b68e9d976aa180e5c615a96db7e9ce05cb25e9
unknown <ms@.(none)>
2011-12-25 21:05:41 -0500

    Create a new Engine object for test

--- SparkPlug/Sparkplug.cpp
+++ SparkPlug/Sparkplug.cpp
@@ -1,10 +1,13 @@
 #include "Sparkplug.h"
-#include "Engine.h"
+#include "Engine/Engine.h"
 
 SparkPlug::SparkPlug(QWidget *parent, Qt::WFlags flags) : 
 QMainWindow(parent, flags) {
 	
+	// Setup the UI using the Qt UI thing
 	ui.setupUi(this);
+	
+	Engine* pEngine= new Engine();
 }
 
 SparkPlug::~SparkPlug() {
